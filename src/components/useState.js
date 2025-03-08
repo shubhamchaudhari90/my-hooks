895cc37971aafcd69b82831373a8f36717a388e3
AB-shubhamchaudhari <schaudhari@accurate.com>
2025-03-08 18:22:08 +0530

    Update codewith Object update

--- src/components/useState.js
+++ src/components/useState.js
@@ -9,15 +9,24 @@ function UseStateDemo() {
   ];
 
   const [color, setColor] = useState('red');
+  const [carData, setCarData] = useState({
+    carColor: "red",
+    model: "BMW",
+    year: 2023,
+  });
   const changeColor = () => {
     const randomIndex = Math.floor(Math.random() * colorNames.length);
     
     setColor(colorNames[randomIndex]);
+    setCarData((prev)=>{
+      return {...prev,carColor:colorNames[randomIndex]};
+    });
   }
 
   return (
     <div className="App">
       <div style={{ color: `${color}` }}>My color is {color}</div>
+      <div style={{ color: `${carData.carColor}` }}>Car color: {carData.carColor}, model : {carData.model} and year: {carData.year}</div>
       <button onClick={changeColor} >Change Color</button>
     </div>
   );
